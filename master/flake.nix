a2f1950adf6e08473aafcdae7d53a482a3e8648f
hugosenari <hugosenari@gmail.com>
2022-03-12 03:29:40 -0300

    chore: re index mmgeoip

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''MaxMind GeoIP library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mmgeoip-master.flake = false;
+  inputs.src-mmgeoip-master.owner = "FedericoCeratto";
+  inputs.src-mmgeoip-master.ref   = "refs/heads/master";
+  inputs.src-mmgeoip-master.repo  = "nim-mmgeoip";
+  inputs.src-mmgeoip-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mmgeoip-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mmgeoip-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
